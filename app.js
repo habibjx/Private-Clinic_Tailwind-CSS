9d58170fef0dcd46861f9dbaea3d6f58372c66f0
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-24 18:13:13 +0600

    menu working

---
+++ app.js
@@ -0,0 +1,31 @@
+/**
+ * Title: this is a private clinic landing page.
+ * Author: M H R Habib.
+ * Date: Mar 24 2025.
+ */
+
+//Global variables
+
+window.onload = () => {
+    main()
+}
+
+function main(){
+
+    //DOM References
+
+    //Event Listener
+
+}
+
+//Event Handler
+
+// DOM function
+function menu(){
+    const menuContainer = document.getElementById('menu');
+    menuContainer.classList.toggle('opacity-100');
+    menuContainer.classList.toggle('invisible');
+    menuContainer.classList.toggle('h-0');
+}
+
+// Units function 
